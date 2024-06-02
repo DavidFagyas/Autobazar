d669faa2d3522c82796a896cdd92f37592c184ad
DavidFagyas <161598258+DavidFagyas@users.noreply.github.com>
2024-06-02 21:37:41 +0200

    Add files via upload

--- header.php
+++ header.php
@@ -1,12 +1,22 @@
-<?php
-        $menuItems = [
-            'Domov' => 'index.html',
-            'Dostupné autá' => 'dostupne-auta.html',
-            'O nás' => 'onas.html',
-            'KONTAKT' => 'kontakt.html',
-        ];
-        ?>
-        <ul class="navbar-nav">
-            <?php foreach ($menuItems as $name => $url): ?>
-                <li><a class="nav-link" href="<?= htmlspecialchars($url); ?>"><?= htmlspecialchars($name); ?></a></li>
-            <?php endforeach; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+    <style>
+        .header {
+            background-color: #f1f1f1;
+            padding: 20px;
+            text-align: center;
+        }
+        .header a {
+            margin: 0 15px;
+            text-decoration: none;
+            color: #000;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <a href="login.php">Login</a>
+        <a href="register.php">Register</a>
+    </div>
